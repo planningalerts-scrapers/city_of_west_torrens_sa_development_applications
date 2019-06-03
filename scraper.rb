20597620fe8a62849831f8f06b287a2249184f25
Matthew Landauer <mlandauer@gmail.com>
2019-06-04 03:06:56 +1000

    Add handling for weird entry for council_reference

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,10 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
+EpathwayScraper.scrape(
   "https://epathway.wtcc.sa.gov.au/ePathway/Production",
   list_type: :last_30_days
-)
+) do |record|
+  if record["council_reference"] != "Not on file"
+    EpathwayScraper.save(record)
+  end
+end
