7fa7068ea0f5e4d7de7ba69a98594d7db1529013
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:52:31 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-EpathwayScraper.scrape_and_save(
-  "https://epathway.wtcc.sa.gov.au/ePathway/Production",
-  list_type: :last_30_days, state: "SA"
-)
+EpathwayScraper.scrape_and_save_authority(:west_torrens)
